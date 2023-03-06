e1d93b73842b243425ffe9401041e7cdf6dd2a2a
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-03-06 16:41:59 +0900

    feat: UserRepositoryのInterfaceにIDキー検索メソッドを追加

--- pkg/repository/dummy/user.go
+++ pkg/repository/dummy/user.go
@@ -1,6 +1,11 @@
 package dummy
 
-import "github.com/approvers/qip/pkg/domain"
+import (
+	"errors"
+
+	"github.com/approvers/qip/pkg/domain"
+	"github.com/approvers/qip/pkg/utils/id"
+)
 
 type UserRepository struct {
 	data []domain.User
@@ -25,4 +30,25 @@ func (u *UserRepository) CreateUser(user domain.User) error {
 	u.data = append(u.data, user)
 
 	return nil
+}
+
+func (u *UserRepository) FindUsersByInstanceID(id id.SnowFlakeID) ([]domain.User, error) {
+	res := make([]domain.User, 0)
+	for _, v := range u.data {
+		if v.GetInstanceID() == id {
+			res = append(res, v)
+		}
+	}
+
+	return res, nil
+}
+
+func (u *UserRepository) FindUserByID(id id.SnowFlakeID) (*domain.User, error) {
+	for _, v := range u.data {
+		if v.GetID() == id {
+			return &v, nil
+		}
+	}
+
+	return nil, errors.New("NotFound")
 }
--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -1,8 +1,13 @@
 package repository
 
-import "github.com/approvers/qip/pkg/domain"
+import (
+	"github.com/approvers/qip/pkg/domain"
+	"github.com/approvers/qip/pkg/utils/id"
+)
 
 type UserRepository interface {
 	FindUsersByName(name string) ([]domain.User, error)
+	FindUserByID(id id.SnowFlakeID) (*domain.User, error)
+	FindUsersByInstanceID(id id.SnowFlakeID) ([]domain.User, error)
 	CreateUser(u domain.User) error
 }
