d1dfe2523f596eb0c7160e4f82b19abd7f192927
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-03-08 23:58:51 +0900

    feat: 落ちていたテストを修正

--- pkg/activitypub/webfinger_test.go
+++ pkg/activitypub/webfinger_test.go
@@ -1,14 +1,14 @@
 package activitypub
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWebFinger(t *testing.T) {
-	wf := WebFinger("testuser@np.test.laminne33569.net")
+	wf, _ := WebFinger("testuser@np.test.laminne33569.net", "np.test.laminne33569.net")
 
 	ex := `{"subject":"acct:testuser@np.test.laminne33569.net","links":[{"rel":"self","type":"application/activity+json","href":"https://np.test.laminne33569.net/users/testuser"},{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://np.test.laminne33569.net/@testuser"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://np.test.laminne33569.net/authorize-follow?acct={uri}"}]}`
-
 	assert.Equal(t, ex, wf)
 }
