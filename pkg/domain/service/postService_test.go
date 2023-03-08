347a0a10b4149faab36089fc646e3f1eb520b9ec
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-03-08 18:32:52 +0900

    feat: PostService(Domain Service)の実装 (#33)

---
+++ pkg/domain/service/postService_test.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"testing"
+	"time"
+
+	"github.com/approvers/qip/pkg/repository/dummy"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/approvers/qip/pkg/domain"
+)
+
+func TestPostService_Exists(t *testing.T) {
+	testData := *domain.NewPost("112233", "hello hello", domain.Home, "332211", time.Now())
+	testDataArray := make([]domain.Post, 1)
+	testDataArray[0] = testData
+	repo := dummy.NewPostRepository(testDataArray)
+
+	postService := NewPostService(repo)
+
+	// 存在しないとき
+	notExist := domain.NewPost("999999", "helloworld", domain.Home, "332211", time.Now())
+	assert.Equal(t, false, postService.Exists(notExist))
+
+	// 存在するとき
+	exist := testData
+	assert.Equal(t, true, postService.Exists(&exist))
+}
