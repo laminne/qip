64e97b803aff21f14c4af007866adb49e34eff01
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-03-10 14:30:24 +0900

    feat: InstanceRepositoryの実装

---
+++ pkg/repository/instance.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"github.com/approvers/qip/pkg/domain"
+	"github.com/approvers/qip/pkg/utils/id"
+)
+
+type InstanceRepository interface {
+	CreateInstance(i domain.Instance) error
+	FindByID(i id.SnowFlakeID) (*domain.Instance, error)
+}
