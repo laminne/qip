5a1e5539aebbf9ad9139dffd95e14144cde90880
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-03-20 10:52:03 +0900

    fix: ユーザー情報の抜けを修正 (#98)

--- pkg/entity/file.go
+++ pkg/entity/file.go
@@ -3,16 +3,16 @@ package entity
 import "time"
 
 type File struct {
-	ID           string `gorm:"primaryKey;not null"`
-	FileName     string `gorm:"not null"`
-	FilePath     *string
-	FileURL      string `gorm:"not null"`
-	ThumbnailURL *string
+	ID           string     `gorm:"primaryKey;not null"`
+	FileName     string     `gorm:"not null;column:filename"`
+	FilePath     *string    `gorm:"column:filepath"`
+	FileURL      string     `gorm:"not null;column:fileurl"`
+	ThumbnailURL *string    `gorm:"column:thumbnailurl"`
 	Blurhash     string     `gorm:"not null"`
-	IsNSFW       bool       `gorm:"not null"`
-	MimeType     string     `gorm:"not null"`
-	CreatedAt    time.Time  `gorm:"precision:6;not null"`
-	UpdatedAt    *time.Time `gorm:"precision:6;autoUpdateTime"`
+	IsNSFW       bool       `gorm:"not null;column:isnsfw"`
+	MimeType     string     `gorm:"not null;column:mimetype"`
+	CreatedAt    time.Time  `gorm:"precision:6;not null;column:createdat"`
+	UpdatedAt    *time.Time `gorm:"precision:6;autoUpdateTime;column:updatedat"`
 
 	UploaderID string `gorm:"column:uploaderid"`
 	// ↓ 循環参照になるのでポインタにした
